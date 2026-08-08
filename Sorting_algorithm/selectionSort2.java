a34d204cf2d0711cf087010f6cbb09c7aae1b36e
Niraj <mrskyd84@gmail.com>
2026-08-08 10:31:24 +0530

    first commit

---
+++ Sorting_algorithm/selectionSort2.java
@@ -0,0 +1,27 @@
+public class selectionSort2 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
