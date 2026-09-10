363103e048c6a0db25de72e52e3de3645f3f7c6f
Niraj <mrskyd84@gmail.com>
2026-09-10 08:33:54 +0530

    tabulation solution added

--- DP/fib.java
+++ DP/fib.java
@@ -11,10 +11,23 @@ public class fib{
         return f[n];
     }
 
+
+    //  solution by tabulation methode
+
+    public static int fibTabulation(int n){
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n]; // ans
+    }
+
     public static void main(String[] args){
         int n = 6;
         int f[] = new int [n+1];
-        System.out.println(fibn(n,f));
+        System.out.println(fibTabulation(n));
 
     }
 }
